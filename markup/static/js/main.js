454d48df79f739641475baf9ac531797d62abd18
Vadim Novash <novash@tut.by>
2017-03-29 18:39:27 +0300

    reviews block && fonts added

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,2 +1,13 @@
 import '../../components/navigation/navigation';
 import '../../components/tariffs/tariffs';
+import '../../components/reviews/slick.min';
+$(() => {
+    $('.slick').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
+});
