a247392087fbe7784d649133fcc5d66fa0190a9f
Vadim Novash <novash@tut.by>
2017-04-29 16:41:29 +0300

    counters added, some jsand image optimizations

--- markup/components/phone/phone.js
+++ markup/components/phone/phone.js
@@ -1,7 +1,16 @@
 window.onload = function (e) {
     const phoneBtn = document.querySelector('#phone-js');
-    phoneBtn.addEventListener('click', function () {
+    const transferBtn = document.querySelectorAll('.transfer-btn');
+    function formInputFocus() {
         const formField = document.querySelector('#sedi-orderform2-address-from');
-        formField.focus();
-    });
+        const formFieldTo = document.querySelector('#sedi-orderform2-address-to');
+        if (formField && formFieldTo) {
+            formField.value = 'Аэропорт Домодедово';
+            formFieldTo.focus();
+        }
+    }
+    phoneBtn.addEventListener('click', formInputFocus);
+    for (let i = 0; i < transferBtn.length; i++) {
+        transferBtn[i].addEventListener('click', formInputFocus);
+    }
 };
--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,14 +1,14 @@
 import '../../components/navigation/navigation';
 import '../../components/tariffs/tariffs';
-import '../../components/reviews/slick.min';
+// import '../../components/reviews/slick.min';
 import '../../components/phone/phone';
 $(() => {
-    $('.slick').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        autoplay: false,
-        autoplaySpeed: 5000
-    });
+    // $('.slick').slick({
+    //     dots: true,
+    //     infinite: true,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 4,
+    //     autoplay: false,
+    //     autoplaySpeed: 5000
+    // });
 });
